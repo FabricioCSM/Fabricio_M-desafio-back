dfb0bf5b58afa4df09302ef6a36cc6f1f4230616
Fabricio Martins <fabriciocsmartins@gmail.com>
2022-05-29 17:35:18 -0300

    [feat] Implementa swagger para documentação da API

--- project/src/app.ts
+++ project/src/app.ts
@@ -3,12 +3,19 @@ import userRoutes from './routes/userRoute';
 import loginRoutes from './routes/loginRoute';
 import productRoutes from './routes/productRoute';
 import purchaseRoutes from './routes/purchaseRoute';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 
 
 app.use(express.json());
 
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.use(userRoutes);
 app.use(loginRoutes);
---
+++ project/src/routes/index.ts
@@ -0,0 +1,5 @@
+import userRoutes from './userRoute';
+import productRoutes from './productRoute';
+import purchaseRoutes from './purchaseRoute';
+import loginRoutes from './loginRoute';
+
