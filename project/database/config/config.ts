417aeb0a0fe00dd1c5fa9c667959021e05b187aa
Fabricio Martins <fabriciocsmartins@gmail.com>
2022-05-30 08:45:59 -0300

    Update config.ts

--- project/database/config/config.ts
+++ project/database/config/config.ts
@@ -9,27 +9,27 @@ const environment = {
     password: process.env.DB_PASSWORD as string,
     database: process.env.DB_DATABASE as string,
     host: process.env.HOSTNAME,
-    dialect: 'qesh',
+    dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USERNAME as string,
     password: process.env.DB_PASSWORD as string,
     database: process.env.DB_DATABASE as string,
     host: process.env.HOSTNAME,
-    dialect: 'qesh',
+    dialect: 'mysql',
   },
   production: {
     username: process.env.DB_USERNAME as string,
     password: process.env.DB_PASSWORD as string,
     database: process.env.DB_DATABASE as string,
     host: process.env.DB_HOST,
-    dialect: 'qesh',
+    dialect: 'mysql',
   }
 }
 
 const sequelizeConnection = new Sequelize(environment.development.database, environment.development.username, environment.development.password, {
   host: environment.development.host,
-  dialect: 'qesh'
+  dialect: 'mysql'
 })
 
 export default sequelizeConnection;
